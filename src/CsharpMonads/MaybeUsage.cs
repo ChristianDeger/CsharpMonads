ed2d30db79fbb26a77e09ceba98cf62057885bdf
Christian Deger <christian@deger.eu>
2010-06-20 19:52:06 +0200

    Usage for extension methods added

--- src/CsharpMonads/MaybeUsage.cs
+++ src/CsharpMonads/MaybeUsage.cs
@@ -27,5 +27,49 @@ namespace CsharpMonads
 
             result.Some.ShouldBeEqualTo(6);
         }
+
+        [Fact]
+        public void UsingExtensionMethodForBinaryOperations()
+        {
+            var m1 = Maybe.From(3);
+            var m2 = Maybe.From(5);
+
+            var result = m1.Operate(m2, (x, y) => x * y);
+
+            result.Some.ShouldBeEqualTo(15);
+        }
+
+        [Fact]
+        public void UsingExtensionMethodToAdd()
+        {
+            var m1 = Maybe.From(1);
+            var m2 = Maybe.From(5);
+
+            var result = m1.Add(m2);
+
+            result.Some.ShouldBeEqualTo(6);
+        }
+
+        [Fact]
+        public void AddNoneToSome()
+        {
+            var mSome = Maybe.From(1);
+            var mNone = Maybe<int>.None;
+
+            var result = mNone.Add(mSome);
+
+            result.IsNone.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void AddSomeToNone()
+        {
+            var mSome = Maybe.From(1);
+            var mNone = Maybe<int>.None;
+
+            var result = mSome.Add(mNone);
+
+            result.IsNone.ShouldBeTrue();
+        }
     }
 }
