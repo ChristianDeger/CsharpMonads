4754f74a4335f574e3ba16834b5b14957e35bedc
Christian Deger <christian@deger.eu>
2010-06-20 19:19:34 +0200

    MaybeBind done

--- src/CsharpMonads/MaybeBind.cs
+++ src/CsharpMonads/MaybeBind.cs
@@ -5,14 +5,26 @@ namespace CsharpMonads
     public static class MaybeBind
     {
         /// <summary>
-        /// Signature copied from IEnumerable
+        ///   Signature copied from IEnumerable
         /// </summary>
         public static Maybe<TResult> SelectMany<TSource, TMaybe, TResult>(
             this Maybe<TSource> source,
             Func<TSource, Maybe<TMaybe>> maybeSelector,
             Func<TSource, TMaybe, TResult> resultSelector)
         {
-            return Maybe<TResult>.None;
+            if (source.IsNone)
+            {
+                return Maybe<TResult>.None;
+            }
+
+            var maybe = maybeSelector(source.Some);
+            if (maybe.IsNone)
+            {
+                return Maybe<TResult>.None;
+            }
+
+            var result = resultSelector(source.Some, maybe.Some);
+            return Maybe.From(result);
         }
     }
 }
--- src/CsharpMonads/MaybeBindSpecs.cs
+++ src/CsharpMonads/MaybeBindSpecs.cs
@@ -2,8 +2,10 @@ namespace CsharpMonads
 {
     using Xunit;
 
+    // All specs with binding using addition of ints.
+
     [Concern(typeof(MaybeBind))]
-    public class when_binding_a_maybe_none_with_maybe_none : StaticContextSpecification
+    public class when_binding_maybe_none_with_maybe_none : StaticContextSpecification
     {
         private Maybe<int> _maybeNone1;
         private Maybe<int> _maybeNone2;
@@ -24,6 +26,81 @@ namespace CsharpMonads
         public void should_return_maybe_none()
         {
             _result.IsNone.ShouldBeTrue();
+        }
+    }
+
+    [Concern(typeof(MaybeBind))]
+    public class when_binding_maybe_none_with_maybe_some : StaticContextSpecification
+    {
+        private Maybe<int> _maybeNone;
+        private Maybe<int> _maybeSome;
+        private Maybe<int> _result;
+
+        protected override void EstablishContext()
+        {
+            _maybeNone = Maybe<int>.None;
+            _maybeSome = Maybe<int>.From(1);
+        }
+
+        protected override void Because()
+        {
+            _result = _maybeNone.SelectMany(m => _maybeSome, (x, y) => x + y);
+        }
+
+        [Observation]
+        public void should_return_maybe_none()
+        {
+            _result.IsNone.ShouldBeTrue();            
+        }
+    }
+
+    [Concern(typeof(MaybeBind))]
+    public class when_binding_maybe_some_with_maybe_none : StaticContextSpecification
+    {
+        private Maybe<int> _maybeNone;
+        private Maybe<int> _maybeSome;
+        private Maybe<int> _result;
+
+        protected override void EstablishContext()
+        {
+            _maybeNone = Maybe<int>.None;
+            _maybeSome = Maybe<int>.From(1);
+        }
+
+        protected override void Because()
+        {
+            _result = _maybeSome.SelectMany(m => _maybeNone, (x, y) => x + y);
+        }
+
+        [Observation]
+        public void should_return_maybe_none()
+        {
+            _result.IsNone.ShouldBeTrue();            
+        }
+    }
+
+    [Concern(typeof(MaybeBind))]
+    public class when_binding_maybe_some_with_maybe_some : StaticContextSpecification
+    {
+        private Maybe<int> _maybeSome1;
+        private Maybe<int> _maybeSome2;
+        private Maybe<int> _result;
+
+        protected override void EstablishContext()
+        {
+            _maybeSome1 = Maybe<int>.From(1);
+            _maybeSome2 = Maybe<int>.From(2);
+        }
+
+        protected override void Because()
+        {
+            _result = _maybeSome1.SelectMany(m => _maybeSome2, (x, y) => x + y);
+        }
+
+        [Observation]
+        public void should_return_maybe_some_with_calculated_value()
+        {
+            _result.Some.ShouldBeEqualTo(3);
         }
     }
 }
